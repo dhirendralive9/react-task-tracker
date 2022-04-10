62b1306f7bce8e64b2c6853a1bd0ab479d544a74
Dhirendra Biswal <dhirendrabiswal9@live.com>
2022-04-10 17:27:06 +0530

    dtate & useState Hook

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,8 +1,22 @@
-const tasks = []
-
+import { useState  } from "react"
 
 const Tasks = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 0,
+      text: 'Go shopping',
+      day: 'Feb 5th at 2:30pm',
+      reminder:false
+    },
+    {
+      id: 1,
+      text: 'Doctor Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder:false
+    }
+  ])
   return (
+    
     <div>
         {tasks.map(
           (task)=>
